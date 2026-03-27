b6631e54b64c41ecaf9b1191217fecd32c179581
milababicwork@gmail.com <milababicwork@gmail.com>
2026-03-27 19:03:35 -0400

    Initial commit

---
+++ src/main/java/com/musicalnote/musicalnote/MusicalnoteApplication.java
@@ -0,0 +1,13 @@
+package com.musicalnote.musicalnote;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicalnoteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MusicalnoteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/musicalnote/musicalnote/MusicalnoteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.musicalnote.musicalnote;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicalnoteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
